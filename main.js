1a7e82fc8eb6a4134681abad66f5fbc217dc0606
dhanvi-23 <84614225+dhanvi-23@users.noreply.github.com>
2022-06-19 13:43:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 song="";
-
 scoreLeftWrist=0;
+scoreRightWrist=0;
+
 
 leftwristX=0;
 leftwristY=0;
@@ -51,8 +52,41 @@ function draw(){
     image(video,0,0,600,500);
     fill("#bf6be3");
     stroke("#4e0a6b");
+    if(scoreRightWrist > 0.2){
+        circle(rightwristX,rightwristY,20);
+        if(rightwristY > 0 && rightwristY <=100)
+        {
+            document.getElementById("speed").innerHTML="speed=0.5x";
+            song.rate(0.5);
+
+        }
+
+        else if(rightwristY >100 && rightwristY<=200)
+        {
+            document.getElementById("speed").innerHTML="speed=1x";
+            song.rate(1);
+        }
+
+        else if(rightwristY >200 && rightwristY<=300)
+        {
+            document.getElementById("speed").innerHTML="speed=1.5x";
+            song.rate(1.5);
+        }
+
+        else if(rightwristY >300 && rightwristY<=400)
+        {
+            document.getElementById("speed").innerHTML="speed=2x";
+            song.rate(2);
+        }
+
+        else if(rightwristY >400 && rightwristY<=500)
+        {
+            document.getElementById("speed").innerHTML="speed=2.5x";
+            song.rate(2.5);
+        }
+    }
     if(scoreLeftWrist > 0.2){
-        circle(leftWristX,leftwristY,20);
+        circle(leftwristX,leftwristY,20);
         InNumberleftWristY=Number(leftWristY);
         remove_decimals=floor(InNumberleftWristY);
         volume=remove_decimals/500;
